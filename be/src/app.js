6406f2f6048b9b2b3478509bdc746cf4b73b775b
tthau02 <tranhau201202@gmail.com>
2025-04-04 16:45:11 +0700

    feat: update CORS origin to allow requests from the production URL

--- be/src/app.js
+++ be/src/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://tthshop.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
